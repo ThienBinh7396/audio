565ae2046c58d702a86219b2eadaf828c5753751
mvn-binhhoang-hn <binh.hoang@monstar-lab.com>
2021-10-07 16:44:27 +0700

    add Authorization to allow header

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(express.urlencoded({ extended: false }));
 const allowCrossDomain = function (req, res, next) {
   res.header(
     "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Origin, x-access-token"
+    "Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Origin, x-access-token, Authorization"
   );
   res.setHeader("Access-Control-Allow-Origin", "*");
   next();
